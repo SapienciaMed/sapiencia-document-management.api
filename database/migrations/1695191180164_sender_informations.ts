f1fa23fdffabf84eb2183e7a4c44f6c98d6a6045
Francisco <gaviria.reyes@gmail.com>
2023-09-22 17:02:26 -0500

    Fix. Fixing error in migrations.

--- database/migrations/1695191180164_sender_informations.ts
+++ database/migrations/1695191180164_sender_informations.ts
@@ -29,8 +29,8 @@ export default class extends BaseSchema {
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp("created_at", { useTz: false });
-      table.timestamp("updated_at", { useTz: false });
+      table.timestamp("created_at").defaultTo(this.now());
+      table.timestamp("updated_at").defaultTo(this.now());
     });
   }
 
