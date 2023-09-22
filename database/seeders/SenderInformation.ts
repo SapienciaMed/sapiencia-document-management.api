8063ea593303fa006d1d72c91b1d8b5c8f55e0a3
Francisco <gaviria.reyes@gmail.com>
2023-09-22 09:33:13 -0500

    adding migrations and seeders documents received

---
+++ database/seeders/SenderInformation.ts
@@ -0,0 +1,9 @@
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import SenderInformationFactory from "Database/factories/SenderInformationFactory";
+
+export default class extends BaseSeeder {
+  public async run() {
+    // Write your database queries inside the run method
+    await SenderInformationFactory.createMany(10);
+  }
+}
