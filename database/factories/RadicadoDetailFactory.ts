b62412d86a3d3a5f6964414cfd75e70642b0b060
Francisco <gaviria.reyes@gmail.com>
2023-09-22 09:32:22 -0500

    adding Factories document received

--- database/factories/RadicadoDetailFactory.ts
+++ database/factories/RadicadoDetailFactory.ts
@@ -1,13 +1,20 @@
 import RadicadoDetail from "App/Models/RadicadoDetail";
 import Factory from "@ioc:Adonis/Lucid/Factory";
+import { DateTime } from "luxon";
 
-export default Factory.define(RadicadoDetail, ({ faker }) => {
+export default Factory.define(RadicadoDetail, ({ faker }): any => {
   return {
-    dra_codigo: parseInt(faker.random.numeric(2)),
+    //dra_codigo: parseInt(faker.random.numeric(2)),
     dra_radicado: faker.random.numeric(12),
-    dra_fecha_radicado: faker.date.past(),
+    dra_fecha_radicado: DateTime.fromISO(
+      faker.date.past().toISOString()
+    ).toFormat("yyyy-MM-dd HH:mm:ss"),
     dra_radicado_origen: faker.random.numeric(12),
-    dra_fecha_origen: faker.date.past(),
+    dra_fecha_origen: DateTime.fromISO(
+      faker.date.past().toISOString()
+    ).toFormat("yyyy-MM-dd HH:mm:ss"),
     dra_nombre_apellidos: faker.person.fullName(),
   };
 }).build();
+
+//DateTime.local().toFormat("yyyy-MM-dd HH:mm:ss")
