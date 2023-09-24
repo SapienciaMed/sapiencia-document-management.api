039fa48a73e83ba836d7e9f52d43e9d816e9d871
andreszl <andreszl.devcode@gmail.com>
2023-09-24 17:40:26 -0500

    all fields

--- app/Models/GeneralConfiguration.ts
+++ app/Models/GeneralConfiguration.ts
@@ -17,6 +17,22 @@ export default class GeneralConfiguration extends BaseModel {
   public email_alarm: boolean;
 
   @column({
+      columnName: "CGE_COMPRIMIR_IMAGEN",
+      serializeAs: "compress_image",
+      prepare: (val) => String(val) === "true" ? 1 : 0,
+      serialize: (val) => Boolean(val)
+  })
+  public compress_image: boolean;
+
+  @column({
+      columnName: "CGE_ACTIVAR_RANGO_IP",
+      serializeAs: "ip_range",
+      prepare: (val) => String(val) === "true" ? 1 : 0,
+      serialize: (val) => Boolean(val)
+  })
+  public ip_range: boolean;
+
+  @column({
       columnName: "CGE_DIAS_HABILES",
       serializeAs: "business_days",
       prepare: (val) => String(val) === "true" ? 1 : 0,
@@ -24,8 +40,25 @@ export default class GeneralConfiguration extends BaseModel {
   })
   public business_days: boolean;
 
+  @column({
+      columnName: "CGE_INICIAR_SIN_IMAGEN_INTERNO",
+      serializeAs: "start_without_image_internal",
+      prepare: (val) => String(val) === "true" ? 1 : 0,
+      serialize: (val) => Boolean(val)
+  })
+  public start_without_image_internal: boolean;
+
   @column({ columnName: "CGE_DURACION_ALMUERZO", serializeAs: "lunch_duration" })
-  public lunch_duration: string;
+  public lunch_duration: number;
+
+  @column({ columnName: "CGE_CORREO_ADMINISTRADOR", serializeAs: "admin_email" })
+  public admin_email: string;
+
+  @column({ columnName: "CGE_RUTAPDF_CREATOR", serializeAs: "route_creator_path" })
+  public route_creator_path: string;
+
+  @column({ columnName: "CGE_RUTA_TEMP_PDF", serializeAs: "pdf_temporary_path" })
+  public pdf_temporary_path: string;
 
 
   @column({ columnName: "CGE_TIEMPO", serializeAs: "time" })
@@ -67,9 +100,12 @@ export default class GeneralConfiguration extends BaseModel {
   @column({ columnName: "CGE_DETALLE_REGINVENTARIO", serializeAs: "inventory_record_detail" })
   public inventory_record_detail: number;
 
-  @column({ columnName: "CGE_RADICADO_TEMPORAL", serializeAs: "temporary_settlement" })
-  public temporary_settlement: boolean;
+  @column({ columnName: "CGE_RADICADO_TEMPORAL", serializeAs: "temporary_filing" })
+  public temporary_filing: boolean;
 
-  @column({ columnName: "CGE_RADICADO_TEMPORAL_NORMA", serializeAs: "standard_temporary_filing" })
-  public standard_temporary_filing: boolean;
+  @column({ columnName: "CGE_RADICADO_TEMPORAL_NORMA", serializeAs: "temporary_filing_rules" })
+  public temporary_filing_rules: boolean;
+
+  @column({ columnName: "CGE_CAUSAL_DEVOLUCION_X_CONDICION", serializeAs: "cause_of_return_x_condition" })
+  public cause_of_return_x_condition: number;
 }
