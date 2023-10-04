f8ac7a538a0ddd49ce0195404b381a71ad607ab8
Francisco <gaviria.reyes@gmail.com>
2023-10-04 18:01:18 -0500

    fixing factory

--- database/factories/SenderInformationFactory.ts
+++ database/factories/SenderInformationFactory.ts
@@ -4,7 +4,7 @@ import Factory from "@ioc:Adonis/Lucid/Factory";
 export default Factory.define(SenderInformation, ({ faker }) => {
   return {
     //dra_codigo: parseInt(faker.random.numeric(2)),
-    usr_tipo_identidad: parseInt(faker.random.numeric(1)),
+    usr_tipo_identidad: "CC",
     usr_numero_identidad: faker.random.numeric(12),
     usr_nombre: faker.person.firstName(),
     usr_apellidos: faker.person.lastName(),
@@ -12,7 +12,7 @@ export default Factory.define(SenderInformation, ({ faker }) => {
     usr_genero: faker.number.int({ min: 1, max: 2 }),
     usr_numero_contacto: faker.random.numeric(10),
     usr_celular: faker.random.numeric(10),
-    usr_pais: parseInt(faker.random.numeric(2)),
+    usr_pais: faker.location.countryCode("alpha-3"),
     usr_departamento: parseInt(faker.random.numeric(2)),
     usr_municipio: parseInt(faker.random.numeric(2)),
     usr_barrio: parseInt(faker.random.numeric(2)),
