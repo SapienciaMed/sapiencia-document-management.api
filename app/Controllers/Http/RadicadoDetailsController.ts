7a9b9012c161ab46325fc3b5bd198fb2bb194da1
andreszl <andreszl.devcode@gmail.com>
2023-10-31 05:08:08 -0500

    fix

--- app/Controllers/Http/RadicadoDetailsController.ts
+++ app/Controllers/Http/RadicadoDetailsController.ts
@@ -370,15 +370,15 @@ export default class RadicadoDetailsController {
         });
       }
 
-      const bucketName = 'sapiencia-document-management';
-      const storage = new Storage();
-      const bucket = storage.bucket(bucketName);
+      // const bucketName = 'sapiencia-document-management';
+      // const storage = new Storage();
+      // const bucket = storage.bucket(bucketName);
 
       for (const file of files) {
         const radicado = file.clientName.replace('.pdf', '');
         const uniqueFileName = `${uuidv4()}.pdf`;
         if (file.tmpPath) {
-          await bucket.upload(file.tmpPath, { destination: `${uniqueFileName}`, });
+          // await bucket.upload(file.tmpPath, { destination: `${uniqueFileName}`, });
           await Database.table('attachments_radicados').insert({ ARA_RADICADO: radicado, ARA_PATH: `${uniqueFileName}`, });
         } else {
           return response.status(400).json({
--- database/migrations/1698741809123_attachments_radicados.ts
+++ database/migrations/1698741809123_attachments_radicados.ts
@@ -1,28 +1,18 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'attachments_radicados'
+  protected tableName = 'ARA_ADJUNTOS_RADICADOS'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table
-      .increments("ARA_CODIGO")
-      .unsigned()
-      .primary()
+      .increments("ARA_CODIGO").primary()
       .comment("llave primaria");
 
       table.string("ARA_RADICADO", 20).notNullable();
-      table
-          .foreign("ARA_RADICADO", "fk_rrr_idRadicado_to_dra_radicado")
-          .references("DRA_RADICADO")
-          .inTable("radicado_details")
-          .onDelete("CASCADE")
-          .onUpdate("CASCADE");
-
-
       table.string("ARA_PATH", 255).notNullable();
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp("created_at").defaultTo(this.now());
+      table.timestamp("updated_at").defaultTo(this.now());
     })
   }
 
